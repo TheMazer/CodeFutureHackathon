65890f9ab47d4824a7230fcb5c1bf8f15a579697
Mazer <119494642+TheMazer@users.noreply.github.com>
2024-04-27 02:06:45 +0300

    Level 1 Design

---
+++ levels/tilesets/objects.tsx
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="objects" tilewidth="1144" tileheight="704" tilecount="86" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="154">
+  <image width="704" height="448" source="../../assets/images/tilesets/objects/154.png"/>
+ </tile>
+ <tile id="156">
+  <image width="64" height="64" source="../../assets/images/tilesets/objects/156.png"/>
+ </tile>
+ <tile id="168">
+  <image width="320" height="128" source="../../assets/images/tilesets/objects/168.png"/>
+ </tile>
+ <tile id="169">
+  <image width="320" height="128" source="../../assets/images/tilesets/objects/169.png"/>
+ </tile>
+ <tile id="170">
+  <image width="448" height="256" source="../../assets/images/tilesets/objects/170.png"/>
+ </tile>
+ <tile id="171">
+  <image width="320" height="128" source="../../assets/images/tilesets/objects/171.png"/>
+ </tile>
+ <tile id="172">
+  <image width="320" height="128" source="../../assets/images/tilesets/objects/172.png"/>
+ </tile>
+ <tile id="173">
+  <image width="512" height="704" source="../../assets/images/tilesets/objects/173.png"/>
+ </tile>
+ <tile id="174">
+  <image width="128" height="128" source="../../assets/images/tilesets/objects/174.png"/>
+ </tile>
+ <tile id="175">
+  <image width="128" height="440" source="../../assets/images/tilesets/objects/175.png"/>
+ </tile>
+ <tile id="176">
+  <image width="128" height="440" source="../../assets/images/tilesets/objects/176.png"/>
+ </tile>
+ <tile id="177">
+  <image width="196" height="128" source="../../assets/images/tilesets/objects/177.png"/>
+ </tile>
+ <tile id="178">
+  <image width="320" height="392" source="../../assets/images/tilesets/objects/178.png"/>
+ </tile>
+ <tile id="179">
+  <image width="124" height="206" source="../../assets/images/tilesets/objects/179.png"/>
+ </tile>
+ <tile id="180">
+  <image width="362" height="368" source="../../assets/images/tilesets/objects/180.png"/>
+ </tile>
+ <tile id="181">
+  <image width="1144" height="630" source="../../assets/images/tilesets/objects/181.png"/>
+ </tile>
+ <tile id="182">
+  <image width="54" height="478" source="../../assets/images/tilesets/objects/182.png"/>
+ </tile>
+ <tile id="183">
+  <image width="96" height="48" source="../../assets/images/tilesets/objects/183.png"/>
+ </tile>
+ <tile id="184">
+  <image width="196" height="196" source="../../assets/images/tilesets/objects/184.png"/>
+ </tile>
+ <tile id="185">
+  <image width="196" height="196" source="../../assets/images/tilesets/objects/185.png"/>
+ </tile>
+ <tile id="186">
+  <image width="64" height="64" source="../../assets/images/tilesets/objects/186.png"/>
+ </tile>
+ <tile id="187">
+  <image width="362" height="368" source="../../assets/images/tilesets/objects/187.png"/>
+ </tile>
+</tileset>
