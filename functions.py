3199b1a08b560b6b269d4f58f2d6bda6ea711dc5
Mazer <119494642+TheMazer@users.noreply.github.com>
2024-04-27 05:14:41 +0300

    Class Config

--- functions.py
+++ functions.py
@@ -2,6 +2,40 @@ from settings import *
 
 from csv import reader
 from os import walk
+import configparser
+
+
+class Config:
+    def __init__(self, path = 'config.ini'):
+        self.path = path
+        self.config = configparser.ConfigParser()
+        self.config.read(self.path)
+
+    def load(self, path = None):
+        self.path = path if path is not None else self.path
+        self.config.read(self.path)
+
+    def get(self, key, section = None):
+        if section is not None:
+            try: value = self.config[section][key]
+            except KeyError: value = 0
+            return value
+        else:
+            try: value = self.config[key]
+            except KeyError: value = 0
+            return value
+
+    def set(self, key, value, section = None):
+        if section is not None:
+            if section not in self.config:
+                self.config[section] = {}
+            self.config[section][key] = value
+        else:
+            self.config[key] = value
+
+    def save(self):
+        with open(self.path, 'w') as configfile:
+            self.config.write(configfile)
 
 
 def importFolder(path):
